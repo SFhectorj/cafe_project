eb85b272b3ba24dec53b57cf50d61f89b57217d4
Baeza Hector <baezahectorj@gmail.com>
2026-07-01 18:07:36 +0000

    website setup

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+import git
+from flask import Flask, render_template, url_for, flash, redirect, request
 from flask_behind_proxy import FlaskBehindProxy
+
 
 app = Flask(__name__)
 proxied = FlaskBehindProxy(app)  ## add this line
@@ -37,6 +39,16 @@ def contact():
         "contact.html",
         title="Contact"
     )
+
+@app.route("/update_server", methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('/home/CHANGE_TO_PYTHON_ANYWHERE_USERNAME/CHANGE_TO_GITHUB_REPO_NAME')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
 
 
 if __name__ == "__main__":
